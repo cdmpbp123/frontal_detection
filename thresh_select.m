fb0f79b42b93ad86b1c51e61223569d81d51a968
cdmpbp123 <359304296@qq.com>
2020-04-12 14:58:41 +0800

    update thresh_select.m

--- thresh_select.m
+++ thresh_select.m
@@ -2,12 +2,23 @@ function [lowThresh, highThresh] = thresh_select(thresh, LowFreq,HighFreq,magGra
 %
 %   Local Function : thresh_select
 %
-if nargin < 6
-    figname = [];
-end
 [m,n] = size(magGrad);
 % Select the thresholds
 if isempty(thresh)
+    [lowThresh, highThresh] = calculate_thresh_by_histogram(LowFreq,HighFreq,magGrad);
+elseif length(thresh)==1
+    highThresh = thresh;
+    lowThresh = ThresholdRatio*thresh;
+elseif length(thresh)==2
+    lowThresh = thresh(1);
+    highThresh = thresh(2);
+    if (lowThresh >= highThresh) || (highThresh >= 1)
+        error(message('images:edge:thresholdOutOfRange'))
+    end
+end
+end
+
+function [lowThresh, highThresh] = calculate_thresh_by_histogram(LowFreq,HighFreq,magGrad)
     nonan_number = numel(find(~isnan(magGrad)));
     [b x]=hist(magGrad(:),100);
     cum_freq=cumsum(b/nonan_number);
@@ -19,14 +30,4 @@ if isempty(thresh)
     c1 = find(cum_freq>HighFreq,1,'first');
     highThresh = interp1(cum_freq(c0:c1),x(c0:c1),HighFreq);
     clear c0 c1
-elseif length(thresh)==1
-    highThresh = thresh;
-    lowThresh = ThresholdRatio*thresh;
-elseif length(thresh)==2
-    lowThresh = thresh(1);
-    highThresh = thresh(2);
-    if (lowThresh >= highThresh) || (highThresh >= 1)
-        error(message('images:edge:thresholdOutOfRange'))
-    end
-end
 end
